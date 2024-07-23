458d099f869a0653088fab18d6ec6a8a551bc8e8
hvtienprotv84 <dmx031117@gmail.om>
2024-07-24 05:39:58 +0700

    push

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className='container_logo'> 
+          <img id="react" src="https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png" className="App-logo" alt="logo" />
+          <img id='vercel' src="https://static-00.iconduck.com/assets.00/vercel-icon-512x449-3422jidz.png" className="App-logo" alt="logo" />
+        </div>
+        <p>
+          First Deploy Vercel - ReactJS
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
